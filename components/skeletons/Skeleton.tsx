cf18ab2aeea7b1e0b784a39817e2ab528167455b
Rishabh Kesharwani <114279839+rishabhkesharwani10@users.noreply.github.com>
2025-10-05 14:21:07 +0530

    Remove all files from repository

--- components/skeletons/Skeleton.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const Skeleton: React.FC<{ className?: string }> = ({ className }) => (
-  <div className={`bg-black/40 animate-pulse rounded-md ${className}`} />
-);
-
-export default Skeleton;
--- pages/SitemapPage.tsx
+++
@@ -1,2 +0,0 @@
-// This component is obsolete and has been removed.
-// The application uses a static sitemap.xml file in the root directory.
